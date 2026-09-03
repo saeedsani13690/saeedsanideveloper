82f766e018692bf1c00e188813c57a150783ac2e
coder ghost <saeedsani1369@email.com>
2026-09-03 14:27:49 +0330

    Add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://webnaro.ir/",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
